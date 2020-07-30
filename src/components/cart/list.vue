dd0cc193905bb0a52312b338109a35af7305b4be
1160300919 <1034538932@qq.com>
2020-07-30 09:41:52 +0800

    加入一部分购物车

---
+++ src/components/cart/list.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="box">
+		<div class="box-header with-border">
+		  <h3 class="box-title">购物车</h3>	          
+		</div>
+		<div class="box-body">
+			<table class="table table-bordered">
+			  <thead>
+				<tr>
+				  <th scope="col">商品名</th>
+				  <th scope="col">ID</th>
+				  <th scope="col">姓名</th>
+				  <th scope="col">操作</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				<tr v-for="dm in userList" v-bind:key="dm.no">
+				  <td>{{dm.no}}</td>
+				  <td>{{dm.id}}</td>
+				  <td>{{dm.name}}</td>
+					<td>
+					  <router-link v-bind:to="'/user/view/'+dm.no" class="btn btn-default">查看详细信息</router-link>
+					  <a href="#" v-on:click="deleteUser(dm.no)"  class="btn btn-danger">删除</a> 
+					</td>
+				</tr>
+			  </tbody>
+			</table>
+		</div>
+		<!-- /.box-body -->
+		<router-link to="/user/add" class="btn btn-default">增加用户</router-link>
+	</div>
+	
+</template>
+
+<script>
+	//import axios from "axios";
+	export default {
+		name:"UserList",
+		data(){
+			return {
+				userList:[],
+				page:1,
+				rows:10,
+				count:0,
+				pageCount:0
+			};
+		},
+		created(){ //当前组件的生命周期方法，组件创建后
+			this.getList();
+		},
+		methods:{
+			getList(){
+				this.axiosJSON.get("/user/list/all/page",{
+					params:{
+						rows:this.rows,
+						page:this.page
+					}
+				}).then(result=>{
+					this.userList=result.data.list;
+					this.count=result.data.count;
+					this.pageCount=result.data.pageCount;
+				});
+			},
+			deleteUser(no){
+				let checkresult=confirm("您确认要删除此用户么");
+				if(checkresult){
+					this.axiosJSON.post("/user/delete",{no:no}).then(result=>{
+						alert(result.data.message);
+						if(result.data.status=="OK"){
+							this.getList();
+						}
+					});
+				}
+				
+				
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ src/components/cart/main.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+	<RouterView></RouterView>	
+</div>
+
+</template>
+
+<script>
+	//购物车的主组件
+	export default {
+		name:"CartMain",
+		data(){
+			return {
+				
+			};
+		},
+		created(){ //当前组件的生命周期方法，组件创建后
+			
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style>
+</style>
