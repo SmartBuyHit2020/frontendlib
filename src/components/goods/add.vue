df18fa65f2ca49f1512da075fa9bb6ba35f7bdab
longzix <1371145761@qq.com>
2020-07-30 11:32:17 +0800

    龙梓鑫增加商品类型下拉菜单

--- src/components/goods/add.vue
+++ src/components/goods/add.vue
@@ -7,9 +7,28 @@
 			<form method="post" v-on:submit.prevent="add()">
 				<div class="form-group">
 					
-					<label for="exampleInputId1">商品分类</label>
-					<input type="text" class="form-control" v-model="goods.itemid" required>
+					
+					<div class="dropdown">
+					  <button class="dropbtn">商品分类</button>
+					  <div class="dropdown-content">
+						  <ul v-for="item in itemList" v-bind:key="item.id">
+						      <li> 
+						  	 <a href="#" v-on:click="setGoodsItemid(item.id)"  >{{item.name}}</a>
+						  	</li>
+						    			
+						  		
+						  </ul>
+					   
+					  </div>
+					</div>
+					
+					
+					<input type="text" class="form-control" v-model="itemname" required>
 					<small id="emailHelp" class="form-text text-muted"></small>
+					
+					
+				
+					
 				</div>
 				<div class="form-group">
 					<label for="exampleInputName1">商品名称</label>
@@ -43,6 +62,8 @@
 		name: "GoodsAdd",
 		data() {
 			return {
+				itemList:[],
+				itemname:'食品',
 				goods: {
 					id: '',
 					itemid: '',
@@ -54,7 +75,30 @@
 				}
 			}
 		},
+		created(){ //当前组件的生命周期方法，组件创建后
+		
+			this.getItemList();
+		},
 		methods:{
+			getItemList(){
+				this.axiosJSON.get("/item/list/all"
+				).then(result=>{
+					this.itemList=result.data.list;
+					
+				});
+			},
+			getItemName(id){
+				for (var i=0;i<=this.itemList.length;i++) {
+					console.log(this.itemList);
+					if(this.itemList[i].id==id)
+					this.itemname=this.itemList[i].name;
+				}
+				
+			},
+			setGoodsItemid( id){
+				this.goods.itemid=id;
+				this.getItemName(id);
+			},
 			add(){
 				this.axiosJSON.post("/goods/add",this.goods).then(result=>{
 					if(result.data.status=="OK"){
@@ -73,4 +117,49 @@
 </script>
 
 <style>
+	/* 下拉按钮样式 */
+	.dropbtn {
+	    background-color: #4CAF50;
+	    color: white;
+	    padding: 16px;
+	    font-size: 16px;
+	    border: none;
+	    cursor: pointer;
+	}
+	
+	/* 容器 <div> - 需要定位下拉内容 */
+	.dropdown {
+	    position: relative;
+	    display: inline-block;
+	}
+	
+	/* 下拉内容 (默认隐藏) */
+	.dropdown-content {
+	    display: none;
+	    position: absolute;
+	    background-color: #f9f9f9;
+	    min-width: 160px;
+	    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+	}
+	
+	/* 下拉菜单的链接 */
+	.dropdown-content a {
+	    color: black;
+	    padding: 12px 16px;
+	    text-decoration: none;
+	    display: block;
+	}
+	
+	/* 鼠标移上去后修改下拉菜单链接颜色 */
+	.dropdown-content a:hover {background-color: #f1f1f1}
+	
+	/* 在鼠标移上去后显示下拉菜单 */
+	.dropdown:hover .dropdown-content {
+	    display: block;
+	}
+	
+	/* 当下拉内容显示后修改下拉按钮的背景颜色 */
+	.dropdown:hover .dropbtn {
+	    background-color: #3e8e41;
+	}
 </style>
